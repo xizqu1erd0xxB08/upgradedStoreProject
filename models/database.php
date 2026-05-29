7063d279b8d006d4dc43da90b270fe4a14f2a6e3
Juan Izquierdo <jdizquierdob080708@outlook.com>
2026-05-28 23:46:00 -0500

    Refactorizar el archivo 'database.php', convirtiéndolo en un archivo con código más útil y ordenado que permite a la conexión lanzar excepciones, a los arrays volverlos asociativos (más legibles) y hacer que los prepared statements sean a nivel de SQL. También, verifica los tipos de cada parámetro para evitar posibles errores en los controladores. Además de retornar métodos útiles como obtener la conexión y cerrarla.

--- models/database.php
+++ models/database.php
@@ -2,13 +2,18 @@
 // Clase Database mejorado con PDO 
 class Database {
     /* Propiedad privada para la conexión porque solo Database necesita manejar la conexión. 
-    Las clases hijas (Product, User, Sale) recibirán la conexión ya lista. */
-    private $connection;
+    Las clases hijas (Product, User, Sale) recibirán la conexión ya lista. 
+    Al usar ?PDO se le dice a PHP que la propiedad puede contener un objeto PDO o ser null si
+    la conexión llega a fallar en el bloque try-catch. Al poner = null; se le está asignando 
+    un valor por defecto al arrancar. Como todavía no se ha ejecutado el constructor, la conexión 
+    empieza vacía (null) */
+    private ?PDO $connection = null;
 
-    // Constructor que junta los parámetros para realizar la conexión a la base de datos:
-    public function __construct($hostName, $hostAdmin, $hostAdminPassword, $databaseName) {
+    /* Constructor que junta los parámetros para realizar la conexión a la base de datos (todos los 
+    parámetros de configuración de la base de datos son estrictamente cadenas de texto, es decir, string) */ 
+    public function __construct(string $hostName, string $hostAdmin, string $hostAdminPassword, string $databaseName) {
         // Construir el Data Source Name (DSN)
-        $dsn = "mysql:host=$hostName;dbname=$databaseName;charset=utfmb4";
+        $dsn = "mysql:host=$hostName;dbname=$databaseName;charset=utf8mb4";
 
         /* charset=utfmb4 es importante porque soporta emojis y caracteres especiales, utf8 normal 
         solamente soporta hasta 3 bytes por caracter; utf8mb4 soporta 4 */
@@ -36,13 +41,17 @@ class Database {
         } 
     }
 
-    // Método público que retorna la conexión a la base de datos:
-    public function getConnection(){
+    /* Método público que retorna la conexión a la base de datos.
+    Declarar ': ?PDO' como tipo de retorno. Significa que este método devuelve objeto de conexión PDO 
+    o devuelve 'null' si hubo error */
+    public function getConnection(): ?PDO {
         return $this->connection;
     }
 
-    // Método público que cierra la conexión a la base de datos:
-    public function closeConnection(){
+    /* Método público que cierra la conexión a la base de datos.
+    Usar ': void' porque este método realiza una acción pero no retorna ningún valor, ': void' es el
+    estándar de PHP para funciones que destruyen o limpian recursos sin devolver resultados */
+    public function closeConnection(): void {
         /* En PDO, para cerrar la conexión simplemente se le asigna null al objeto. PHP
         libera los recursos automáticamente */
         $this->connection = null;
