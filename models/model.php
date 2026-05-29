6b1e8ff56ff7da0b7462288ea64510b794653a11
Juan Izquierdo <jdizquierdob080708@outlook.com>
2026-05-28 23:38:29 -0500

    Crear el archivo 'model.php' y dentro del mismo la clase 'Model' para que sea la clase padre de todos los modelos que requieran la conexión a la base de datos, sin necesidad de repetir en cada archivo el código que inicialice el constructor de la conexión

--- models/model.php
+++ models/model.php
@@ -3,12 +3,11 @@
 que obtengan la conexión, es decir, para que la hereden */
 
 class Model {
-    // 'protected' para que los demás modelos puedan acceder a esta conexión
-    /** @var PDO  */
-    protected $connection;
-
-    // Todas las clases hijas heredan este constructor automáticamente
-    public function __construct($connection)
+    /* 'protected' para que los demás modelos puedan acceder a esta conexión. 
+    Al poner 'protected PDO $connection' ya está perfectamente documentado y tipado */
+    protected PDO $connection;
+ 
+    public function __construct(PDO $connection) // <- Type hint de la firma (PDO $connection)
     {
         $this->connection = $connection;
     }
