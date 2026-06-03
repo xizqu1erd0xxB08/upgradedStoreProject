ecd39b06cf68f4ffd76481f62c3f2a8f3bf82fa5
Juan Izquierdo <jdizquierdob080708@outlook.com>
2026-06-03 17:56:24 -0500

    Refactorizar archivo para pasar los nombres de variables 'camelCase'

--- controllers/updateProductController.php
+++ controllers/updateProductController.php
@@ -1,54 +1,58 @@
 <?php 
+// REFACTORIZAR CONTROLLER A PDO 
 session_start();
 
-// Evitar caché para el botón de atrás
+// Evitar caché del navegador (botón atrás)
 header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
 
-require_once '../config.php';
-require_once '../models/Database.php';
-require_once '../models/Product.php';
+require_once dirname(__DIR__, 1) . '/config.php';
+require_once dirname(__DIR__, 1) . '/models/Database.php';
+require_once dirname(__DIR__, 1) . '/models/Product.php';
 
-// Validar sesión
-if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_name'])) {
+// Validar sesión del usuario (si no tiene la sesión activa, redirigir a la vista del log in)
+if (!isset($_SESSION['userId']) || !isset($_SESSION['userName']) || !isset($_SESSION['rolesArray'])) {
     header("Location: ../views/loginView.php");
-    exit();
+    exit();    
 }
 
-$user_id = $_SESSION['user_id']; // Variable necesaria 
+// Obtener la id de sesión del usuario
+$userId = $_SESSION['userId'];
 
 // Validar que los datos del formulario pre-llenado hayan llegado correctamente mediante el método POST
-if (!isset($_POST['product_name']) || !isset($_POST['product_price']) || !isset($_POST['current_stock']) || !isset($_POST['product_id'])) {
+if (!isset($_POST['productName']) || !isset($_POST['productPrice']) || !isset($_POST['currentStock']) || 
+!isset($_POST['productId'])) {
     die("Datos del formulario para editar producto no encontrados");
 }
 
 // Definir variables necesarias para llevar a cabo el método
-$product_name = $_POST['product_name'];
-$product_price = $_POST['product_price'];
-$current_stock = $_POST['current_stock'];
-$product_id = $_POST['product_id'];
+$productName = $_POST['productName'];
+$productPrice = $_POST['productPrice'];
+$currentStock = $_POST['currentStock'];
+$productId = $_POST['productId'];
 
-$database = new Database($host_name, $host_admin, $host_admin_password, $database_name); // Crear objeto database
+// Crear la conexión
+$database = new Database($hostName, $hostAdmin, $hostAdminPassword, $databaseName);
+$connection = $database->getConnection();
 
-$connection = $database->getConnection(); // Obtener conexión
-
-// Verificar conexión
-if (!$connection) {
-    die("Conexión fallida a la base de datos:");
+// Ahora, con PDO, si la conexión falló el método 'getConnection()' retorna 'null'
+if ($connection === null) {
+    die("No se pudo conectar a la base de datos: " . $database->getConnectionError());
 }
 
-$product = new Product($connection); // Crear objeto $product
+// Crear el objeto $product
+$product = new Product($connection);
 
-$update_product = $product->updateProduct($product_name, $product_price, $current_stock, $user_id, $product_id); // Obtener método updateProduct
+$updateProduct = $product->updateProduct($productName, $productPrice, $currentStock, $userId, $productId);
 
 // Verificar que el método se haya ejecutado correctamente
-if (!$update_product['success']) {
+if (!$updateProduct['success']) {
     $database->closeConnection();
-    die($update_product['errorMessage']);
+    die($updateProduct['errorMessage']);
 }
 
-// Éxito
+// Éxito, cerrar conexión y redirigir a la vista de productos
 $database->closeConnection();
 header("Location: viewProductsController.php");
 exit();
