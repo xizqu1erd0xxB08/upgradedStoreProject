898520b66cfa4f8453f49da5511a0df308d64b82
Juan Izquierdo <jdizquierdob080708@outlook.com>
2026-06-03 12:00:21 -0500

    Mejorar el formulario de vista para el inicio de sesión al sistema, incluir verificación de que no haya un flashError proveniente del controlador de registro de usuarios privado. Mostrar opción de iniciar sesión con nombre de usuario o correo electrónico. Añadir opción de que, si el usuario no tiene cuenta, lo redirija directamente al formulario de registro de usuario con un mensaje amigable que diga: '¿No tienes cuenta? Regístrate'

--- views/loginView.php
+++ views/loginView.php
@@ -8,6 +8,10 @@
 </head>
 <body>
     <h2>Login</h2>
+    <!-- Mostrar el mensaje de prohibición al usuario para acceder a un formulario protegido -->
+    <?php if (isset($_SESSION['flashError'])): ?>
+        <p style="color: red;"> <?php echo $_SESSION['flashError']; unset($_SESSION['flashError']); ?></p>
+    <?php endif; ?>
     <form action="../controllers/loginController.php" method="post" class="login-form">
         <label for="userIdentifier">Nombre de usuario o Correo electrónico:</label>
         <input type="text" id="userIdentifier" name="userIdentifier" required>
@@ -22,6 +26,6 @@
     </form>
 
         <!-- Mejorar loginView.php para que usuarios que no tengan cuenta accedan al link de registro. -->
-         <p>¿No tienes cuenta?</p> <a href="/publicRegisterView.php">Regístrate</a>
+         <p>¿No tienes cuenta? <a href="publicRegisterView.php">Regístrate</a></p> 
 </body>
 </html>
